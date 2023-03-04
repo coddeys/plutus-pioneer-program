41cb5c7048a395122b538b3a25a833f2323b2b5d
Dima S <dima@sukhikh.xyz>
2023-03-04 21:57:05 +0100

    Tweak Week02 scripts, use locale machine paths

--- code/Week02/scripts/collect-gift.sh
+++ code/Week02/scripts/collect-gift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
+assets=~/code/cardano/plutus-pioneer-program/code/Week02/assets
+keypath=~/code/cardano/plutus-pioneer-program/keys
 name="$1"
 collateral="$2"
 txin="$3"
--- code/Week02/scripts/make-gift.sh
+++ code/Week02/scripts/make-gift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
+assets=~/code/cardano/plutus-pioneer-program/code/Week02/assets
+keypath=~/code/cardano/plutus-pioneer-program/keys
 name="$1"
 txin="$2"
 body="$assets/gift.txbody"

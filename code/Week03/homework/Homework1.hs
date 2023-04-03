2024b076cb1b2658389e7a505b23666447d8f12b
Dima S <dima@sukhikh.xyz>
2023-04-03 21:12:34 +0200

    solve HW3-1

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -7,13 +7,18 @@
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
-import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
-import           Utilities            (wrapValidator)
-
+import           Plutus.V1.Ledger.Interval        (contains, member)
+import           Plutus.V2.Ledger.Contexts        (txSignedBy)
+import           Plutus.V2.Ledger.Api             (BuiltinData, POSIXTime, PubKeyHash,
+                                                   TxInfo (txInfoValidRange),
+                                                   scriptContextTxInfo, to, from,
+                                                   ScriptContext, Validator,
+                                                   mkValidatorScript)
+import           PlutusTx                         (compile, unstableMakeIsData)
+import           PlutusTx.Prelude                 (Bool (..), (&&), (||) )
+import           Utilities                        (wrapValidator)
+import Data.Aeson (Value(Bool))
+import PlutusTx.Bool (not)
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
@@ -29,7 +34,35 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator _dat () _ctx =
+  validate1 (beneficiary1 _dat) (deadline _dat) txInfo
+  || validate2 (beneficiary2 _dat) (deadline _dat) txInfo
+
+  where 
+    txInfo :: TxInfo
+    txInfo = scriptContextTxInfo _ctx
+
+    
+
+validate1 :: PubKeyHash -> POSIXTime -> TxInfo ->  Bool
+validate1 b dl tx =
+  txSignedBy tx b && deadlineReached
+
+  where
+    deadlineReached :: Bool
+    deadlineReached = contains (to dl) (txInfoValidRange tx)
+
+
+validate2 :: PubKeyHash -> POSIXTime -> TxInfo ->  Bool
+validate2 b dl tx =
+  txSignedBy tx b && afterDeadline
+
+  where
+    afterDeadline :: Bool
+    afterDeadline =
+      contains (from dl) (txInfoValidRange tx)
+      && (not(member dl (txInfoValidRange tx)))
+
 
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()

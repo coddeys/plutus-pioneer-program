e778e35e3ac373e1e1cf7d5a6eec9e6788fd6dab
Dima S <dima@sukhikh.xyz>
2023-04-11 12:19:44 +0200

    wip

---
+++ code/Week04/lecture/seed.ts
@@ -0,0 +1,1 @@
+export const secretSeed="noble knife spoon major crew vanish wife pink extend casion amateur property evidenc thumb craft goose sister depend pull peanut antenna cabin dragon winner"

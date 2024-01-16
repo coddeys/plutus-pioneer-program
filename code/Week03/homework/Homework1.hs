1ece5bfd08762e57d635378e7083222050eb0e96
Dima S <dima.sukhikh@nedap.com>
2024-01-16 20:16:44 +0100

    Solve HW3-1, write vestingValidator

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -7,16 +7,22 @@
 
 module Homework1 where
 
+import Plutus.V1.Ledger.Interval (contains)
 import Plutus.V2.Ledger.Api
   ( BuiltinData,
     POSIXTime,
+    POSIXTimeRange,
     PubKeyHash,
-    ScriptContext,
+    ScriptContext (scriptContextTxInfo),
+    TxInfo (txInfoValidRange),
     Validator,
+    from,
     mkValidatorScript,
+    to,
   )
+import Plutus.V2.Ledger.Contexts (txSignedBy)
 import PlutusTx (compile, unstableMakeIsData)
-import PlutusTx.Prelude (Bool (..))
+import PlutusTx.Prelude (Bool, traceIfFalse, ($), (&&), (+), (||))
 import Utilities (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -34,7 +40,25 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator dat () ctx =
+  traceIfFalse "Benificiary1 did not sign or to late" checkBenificiary1
+    || traceIfFalse "Benificiary2 did not sign or is to early" checkBenificiary2
+  where
+    txInfo :: TxInfo
+    txInfo = scriptContextTxInfo ctx
+
+    txValidRange :: POSIXTimeRange
+    txValidRange = txInfoValidRange txInfo
+
+    checkBenificiary1 :: Bool
+    checkBenificiary1 =
+      txSignedBy txInfo (beneficiary1 dat)
+        && contains (to (deadline dat)) txValidRange
+
+    checkBenificiary2 :: Bool
+    checkBenificiary2 =
+      txSignedBy txInfo (beneficiary2 dat)
+        && contains (from (1 + deadline dat)) txValidRange
 
 {-# INLINEABLE mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()

274c11c737aa3782b4243b8a93bd60b771c6ef79
Dima S <dima.sukhikh@nedap.com>
2024-01-16 19:20:32 +0100

    format Week03 Homework1

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -1,39 +1,44 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeApplications  #-}
-{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
-import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
-import           Utilities            (wrapValidator)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    POSIXTime,
+    PubKeyHash,
+    ScriptContext,
+    Validator,
+    mkValidatorScript,
+  )
+import PlutusTx (compile, unstableMakeIsData)
+import PlutusTx.Prelude (Bool (..))
+import Utilities (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
 data VestingDatum = VestingDatum
-    { beneficiary1 :: PubKeyHash
-    , beneficiary2 :: PubKeyHash
-    , deadline     :: POSIXTime
-    }
+  { beneficiary1 :: PubKeyHash,
+    beneficiary2 :: PubKeyHash,
+    deadline :: POSIXTime
+  }
 
 unstableMakeIsData ''VestingDatum
 
-{-# INLINABLE mkVestingValidator #-}
+{-# INLINEABLE mkVestingValidator #-}
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
 mkVestingValidator _dat () _ctx = False -- FIX ME!
 
-{-# INLINABLE  mkWrappedVestingValidator #-}
+{-# INLINEABLE mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedVestingValidator = wrapValidator mkVestingValidator
 
 validator :: Validator
-validator = mkValidatorScript $$(compile [|| mkWrappedVestingValidator ||])
+validator = mkValidatorScript $$(compile [||mkWrappedVestingValidator||])

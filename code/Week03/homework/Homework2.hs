6d359413c193278ed894853d5f27488f970b1947
Dima S <dima.sukhikh@nedap.com>
2024-01-17 20:59:22 +0100

    Solve HW3-2, write a parameterized vesting validator

--- code/Week03/homework/Homework2.hs
+++ code/Week03/homework/Homework2.hs
@@ -1,30 +1,52 @@
-{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Homework2 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
-import           PlutusTx             (applyCode, compile, liftCode)
-import           PlutusTx.Prelude     (Bool (False), (.))
-import           Utilities            (wrapValidator)
+import Plutus.V1.Ledger.Interval (contains)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    POSIXTime,
+    POSIXTimeRange,
+    PubKeyHash,
+    ScriptContext (scriptContextTxInfo),
+    TxInfo (txInfoValidRange),
+    Validator,
+    from,
+    mkValidatorScript,
+  )
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import PlutusTx (applyCode, compile, liftCode)
+import PlutusTx.Prelude (Bool, traceIfFalse, (&&), (.))
+import Utilities (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
-{-# INLINABLE mkParameterizedVestingValidator #-}
+{-# INLINEABLE mkParameterizedVestingValidator #-}
 -- This should validate if the transaction has a signature from the parameterized beneficiary and the deadline has passed.
 mkParameterizedVestingValidator :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator _beneficiary _deadline () _ctx = False -- FIX ME!
+mkParameterizedVestingValidator beneficiary deadline () ctx =
+  traceIfFalse "Benificiary did not sign or is to early" checkBenificiary
+  where
+    txInfo :: TxInfo
+    txInfo = scriptContextTxInfo ctx
 
-{-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
+    txValidRange :: POSIXTimeRange
+    txValidRange = txInfoValidRange txInfo
+
+    checkBenificiary :: Bool
+    checkBenificiary =
+      txSignedBy txInfo beneficiary
+        && contains (from deadline) txValidRange
+
+{-# INLINEABLE mkWrappedParameterizedVestingValidator #-}
 mkWrappedParameterizedVestingValidator :: PubKeyHash -> BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedParameterizedVestingValidator = wrapValidator . mkParameterizedVestingValidator
 
 validator :: PubKeyHash -> Validator
-validator beneficiary = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode beneficiary)
+validator beneficiary = mkValidatorScript ($$(compile [||mkWrappedParameterizedVestingValidator||]) `applyCode` liftCode beneficiary)

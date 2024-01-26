ea39cecb7cf308b0dc2400f8383953acd094ffbe
Dima S <dima@sukhikh.xyz>
2024-01-26 20:11:33 +0100

    Week08: set socket_path form local-cluster-info.json plutip

--- code/Week08/scripts/query-tip.sh
+++ code/Week08/scripts/query-tip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export CARDANO_NODE_SOCKET_PATH=/workspace/cardano-private-testnet-setup/private-testnet/node-spo1/node.sock
-cardano-cli query tip --testnet-magic 42
+export CARDANO_NODE_SOCKET_PATH=$(jq -r '.ciNodeSocket' plutip/local-cluster-info.json)
+cardano-cli query tip --mainnet
